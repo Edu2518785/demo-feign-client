c052226ee95e9f2d3c6197d9c87949864a47979f
Edu2518785 <diego.huayra.jaramillo@gmail.com>
2025-03-15 09:37:31 -0500

    Primera actualizacion

---
+++ src/main/java/pe/edu/idat/demo_feign_client/DemoFeignClientApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.demo_feign_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFeignClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoFeignClientApplication.class, args);
+	}
+
+}
